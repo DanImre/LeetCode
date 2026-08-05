6b1a44c7f5470bea248400ed5f70f146ec130173
Dán Imre <dan.imre@qb.hu>
2026-08-05 12:32:27 +0200

    3310 - Medium

--- C#/Medium/3310_Remove_Methods_From_Project.cs
+++ C#/Medium/3310_Remove_Methods_From_Project.cs
@@ -10,9 +10,42 @@ namespace Medium
     {
         public IList<int> RemainingMethods(int n, int k, int[][] invocations)
         {
+            Dictionary<int, List<int>> graph = invocations
+                .GroupBy(x => x[0])
+                .ToDictionary(x => x.Key, x => x.Select(y => y[1])
+                .ToList());
+
             HashSet<int> suspiciousMethods = [];
 
-            Queue<int> queue = new();
+            Queue<int> q = new();
+            q.Enqueue(k);
+
+            while (q.Count != 0)
+            {
+                var curr = q.Dequeue();
+
+                if (!suspiciousMethods.Add(curr))
+                    continue;
+
+                List<int> neighbours = graph.TryGetValue(curr, out var list) ? list : [];
+                foreach (var item in neighbours)
+                    q.Enqueue(item);
+            }
+
+            foreach (var connection in invocations)
+            {
+                // A not suspicious -> suspicious 
+                if (!suspiciousMethods.Contains(connection[0])
+                    && suspiciousMethods.Contains(connection[1]))
+                {
+                    suspiciousMethods = [];
+                    break;
+                }
+            }
+
+            return Enumerable.Range(0, n)
+                .Except(suspiciousMethods)
+                .ToList();
         }
     }
 }
