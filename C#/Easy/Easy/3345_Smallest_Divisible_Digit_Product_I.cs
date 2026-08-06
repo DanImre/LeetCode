4d407f9918c2a80616305a5549d2e8d726277d2d
Dán Imre <dan.imre@qb.hu>
2026-08-06 16:00:11 +0200

    3345 iterative solution

---
+++ C#/Easy/Easy/3345_Smallest_Divisible_Digit_Product_I.cs
@@ -0,0 +1,19 @@
+﻿namespace Easy
+{
+    public class _3345_Smallest_Divisible_Digit_Product_I
+    {
+        public int SmallestNumber(int n, int t)
+        {
+            while (true)
+            {
+                int digitsProduct = n.ToString()
+                    .ToCharArray()
+                    .Aggregate(1, (x, y) => x * (y - '0'));
+
+                if (digitsProduct % t == 0)
+                    return n;
+                n++;
+            }
+        }
+    }
+}
