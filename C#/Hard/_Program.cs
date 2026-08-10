5f9a4ac665780f5e54c8114d61fcbe7a8b3b6826
Dán Imre <dan.imre.17f@gmail.com>
2026-08-10 22:17:13 +0200

    Stone game IV done, V started

--- C#/Hard/_Program.cs
+++ C#/Hard/_Program.cs
@@ -6,8 +6,10 @@ namespace Hard
     {
         static void Main(string[] args)
         {
-            var solver = new _3348_Smallest_Divisible_Digit_Product_II();
-            Console.WriteLine(solver.SmallestNumber("1234", 256));
+            var stoneGameFour = new _1510_Stone_Game_IV();
+            Console.WriteLine(stoneGameFour.WinnerSquareGame(2));
+            //var solver = new _3348_Smallest_Divisible_Digit_Product_II();
+            //Console.WriteLine(solver.SmallestNumber("1234", 256));
         }
 
         public static int[][] DoubleIntArrayFromString(string input)
