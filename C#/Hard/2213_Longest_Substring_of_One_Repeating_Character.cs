babdfddac71473e0a009fb5f3775e61796873f65
Dán Imre <dan.imre@qb.hu>
2026-08-13 14:32:30 +0200

    2213 TLE solution for now

---
+++ C#/Hard/2213_Longest_Substring_of_One_Repeating_Character.cs
@@ -0,0 +1,149 @@
+﻿using System.Runtime.Serialization;
+
+namespace Hard
+{
+    public class _2213_Longest_Substring_of_One_Repeating_Character
+    {
+        // Should create a segment tree
+        public int[] LongestRepeating(string s, string queryCharacters, int[] queryIndices)
+        {
+            char[] sArr = s.ToCharArray();
+            int length = s.Length;
+            int[] seriesLength = new int[length];
+            s += "1";
+            int runningCount = 1;
+            int max = 0;
+            int maxI = -1;
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i] == s[i - 1])
+                {
+                    runningCount++;
+                    continue;
+                }
+
+                if (max < runningCount)
+                {
+                    max = runningCount;
+                    maxI = i - 1;
+                }
+
+                for (int j = 1; j <= runningCount; j++)
+                    seriesLength[i - j] = runningCount;
+
+                runningCount = 1;
+            }
+
+            int[] solution = new int[queryIndices.Length];
+            for (int i = 0; i < queryIndices.Length; i++)
+            {
+                //Console.WriteLine(string.Join(", ", seriesLength));
+                //Console.WriteLine(string.Join(", ", sArr));
+                //Console.WriteLine("-------------------");
+
+                int replaceIndex = queryIndices[i];
+                char replaceChar = queryCharacters[i];
+                char previousChar = sArr[replaceIndex];
+
+                if (replaceChar == previousChar)
+                {
+                    if (i == 0)
+                        solution[i] = max;
+                    else
+                        solution[i] = solution[i - 1];
+
+                    continue;
+                }
+
+                sArr[replaceIndex] = replaceChar;
+
+                int countOnLeft = 0;
+                int leftIndex = replaceIndex - 1;
+                while (leftIndex >= 0 && sArr[leftIndex] == previousChar)
+                {
+                    countOnLeft++;
+                    leftIndex--;
+                }
+                leftIndex++;
+
+                int countOnRight = seriesLength[leftIndex] - countOnLeft - 1;
+
+                while (leftIndex < replaceIndex)
+                {
+                    seriesLength[leftIndex] = countOnLeft;
+                    leftIndex++;
+                }
+
+                leftIndex++;
+
+                while (leftIndex < length && sArr[leftIndex] == previousChar)
+                {
+                    seriesLength[leftIndex] = countOnRight;
+                    leftIndex++;
+                }
+
+                // ------------------ appending
+
+                int totalCount = 1;
+                leftIndex = replaceIndex - 1;
+                while (leftIndex >= 0 && sArr[leftIndex] == replaceChar)
+                {
+                    totalCount++;
+                    leftIndex--;
+                }
+                leftIndex++;
+
+
+                int rightIndex = replaceIndex + 1;
+                while (rightIndex < length && sArr[rightIndex] == replaceChar)
+                {
+                    totalCount++;
+                    rightIndex++;
+                }
+                rightIndex--;
+
+                int runningLeftIndex = leftIndex;
+                while (runningLeftIndex <= rightIndex)
+                {
+                    seriesLength[runningLeftIndex] = totalCount;
+                    runningLeftIndex++;
+                }
+
+                if (totalCount > max)
+                {
+                    solution[i] = totalCount;
+                    max = totalCount;
+                    maxI = rightIndex;
+                    continue;
+                }
+
+                if (maxI < leftIndex || maxI > rightIndex)
+                {
+                    solution[i] = max;
+                    continue;
+                }
+
+                int prevMax = max;
+                max = 0;
+                maxI = -1;
+                for (int j = 0; j < seriesLength.Length; j++)
+                {
+                    if (max >= seriesLength[j])
+                        continue;
+
+                    max = seriesLength[j];
+                    maxI = j;
+
+                    if (prevMax == max)
+                        break;
+                }
+
+                solution[i] = max;
+
+            }
+
+            //Console.WriteLine(string.Join(", ", solution));
+            return solution;
+        }
+    }
+}
--- C#/Hard/_Program.cs
+++ C#/Hard/_Program.cs
@@ -6,41 +6,9 @@ namespace Hard
     {
         static void Main(string[] args)
         {
-            var solver = new Hard_37();
-
-            //char[][] board = new char[][]
-            //{
-            //    new char[] { '5', '3', '.', '.', '7', '.', '.', '.', '.' },
-            //    new char[] { '6', '.', '.', '1', '9', '5', '.', '.', '.' },
-            //    new char[] { '.', '9', '8', '.', '.', '.', '.', '6', '.' },
-            //    new char[] { '8', '.', '.', '.', '6', '.', '.', '.', '3' },
-            //    new char[] { '4', '.', '.', '8', '.', '3', '.', '.', '1' },
-            //    new char[] { '7', '.', '.', '.', '2', '.', '.', '.', '6' },
-            //    new char[] { '.', '6', '.', '.', '.', '.', '2', '8', '.' },
-            //    new char[] { '.', '.', '.', '4', '1', '9', '.', '.', '5' },
-            //    new char[] { '.', '.', '.', '.', '8', '.', '.', '7', '9' }
-            //};
-
-            char[][] board = new char[][]
-            {
-                new char[] { '.', '.', '.', '.', '.', '.', '.', '.', '.' },
-                new char[] { '.', '9', '.', '.', '1', '.', '.', '3', '.' },
-                new char[] { '.', '.', '6', '.', '2', '.', '7', '.', '.' },
-                new char[] { '.', '.', '.', '3', '.', '4', '.', '.', '.' },
-                new char[] { '2', '1', '.', '.', '.', '.', '.', '9', '8' },
-                new char[] { '.', '.', '.', '.', '.', '.', '.', '.', '.' },
-                new char[] { '.', '.', '2', '5', '.', '6', '4', '.', '.' },
-                new char[] { '.', '8', '.', '.', '.', '.', '.', '1', '.' },
-                new char[] { '.', '.', '.', '.', '.', '.', '.', '.', '.' }
-            };
-
-            solver.SolveSudoku(board);
-
-            Console.WriteLine("----------------------------------------");
-            Console.WriteLine("----------------------------------------");
-            Console.WriteLine("----------------------------------------");
-            Console.WriteLine("----------------------------------------");
-            solver.PrintBoard(board);
+            var asd = new _2213_Longest_Substring_of_One_Repeating_Character();
+            Console.WriteLine(string.Join(", ", "babacc".ToArray()));
+            asd.LongestRepeating("babacc", "bcb", [1, 3, 3]);
         }
 
         public static int[][] DoubleIntArrayFromString(string input)
