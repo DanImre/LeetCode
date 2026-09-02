09de9f72f85ac44b9e3184431c3c2221a54eb02a
Dán Imre <dan.imre@qb.hu>
2026-09-02 09:38:57 +0200

    3875 done

---
+++ C#/Easy/Easy/3875_Construct_Uniform_Parity_Array_I.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easy
+{
+    public class _3875_Construct_Uniform_Parity_Array_I
+    {
+        public bool UniformArray(int[] nums1)
+        {
+            return true;
+        }
+    }
+}
