d64470c28befcfb1d88bfb5e9ff3922e79333364
Dán Imre <dan.imre@qb.hu>
2026-09-04 08:50:35 +0200

    3903 done

---
+++ C#/Easy/Easy/3903_Smallest_Stable_Index_I.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easy
+{
+    public class _3903_Smallest_Stable_Index_I
+    {
+        public int FirstStableIndex(int[] nums, int k)
+        {
+            int[] minLookup = new int[nums.Length];
+            int min = int.MaxValue;
+            for (int i = nums.Length-1; i >= 0; i--)
+            {
+                min = Math.Min(min, nums[i]);
+                minLookup[i] = min;
+            }
+
+            int max = -1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                max = Math.Max(max, nums[i]);
+
+                if (max - minLookup[i] <= k)
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+}
