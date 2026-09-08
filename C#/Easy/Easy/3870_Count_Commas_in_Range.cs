e4c1027632e4dd7f56c6fd02c5da90679a080f48
Dán Imre <dan.imre@qb.hu>
2026-09-08 08:30:54 +0200

    3870 done

---
+++ C#/Easy/Easy/3870_Count_Commas_in_Range.cs
@@ -0,0 +1,10 @@
+﻿namespace Easy
+{
+    public class _3870_Count_Commas_in_Range
+    {
+        public int CountCommas(int n)
+        {
+            return Math.Max(n - 999, 0);
+        }
+    }
+}
